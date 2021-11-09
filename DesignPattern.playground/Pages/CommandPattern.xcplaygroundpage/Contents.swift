8095da4104a9d4cb4945924190e7a5697352cef6
Mingu Seo <tjalsrnek@gmail.com>
2021-11-09 18:40:30 +0900

    [add] command pattern

---
+++ DesignPattern.playground/Pages/CommandPattern.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+
+protocol Command {
+    func execute()
+}
+
+public class Light {
+    public func on() { print("Light On") }
+    public func off() { print("Light Off") }
+}
+
+public class LightOnCommand: Command {
+    let light: Light
+    init(light: Light) {
+        self.light = light
+    }
+
+    public func execute() {
+        light.on()
+    }
+}
+
+public class SimpleRemoteControl {
+    var slot: Command?
+    
+    func setCommand(command: Command) {
+        slot = command
+    }
+    
+    func buttonWasPressed() {
+        slot?.execute()
+    }
+}
+
+public class GarageDoor {
+    func up() { print("Garage Door Open")}
+    func down() { print("Garage Door down")}
+    func stop() { print("Garage Door stop")}
+    func lightOn() { print("Garage Door lightOn")}
+    func lightOff() { print("Garage Door lightOff")}
+}
+
+public class GarageDoorOpenCommand: Command {
+    let garageDoor: GarageDoor
+    
+    init(garageDoor: GarageDoor) {
+        self.garageDoor = garageDoor
+    }
+    
+    public func execute() {
+        garageDoor.up()
+    }
+}
+
+let remote = SimpleRemoteControl()
+let light = Light()
+let lightOn = LightOnCommand(light: light)
+let garageDoor = GarageDoor()
+let garageDoorOpen = GarageDoorOpenCommand(garageDoor: garageDoor)
+
+remote.setCommand(command: lightOn)
+remote.buttonWasPressed()
+
+remote.setCommand(command: garageDoorOpen)
+remote.buttonWasPressed()
